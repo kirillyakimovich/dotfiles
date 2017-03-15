273a858de838caecec82540887af8acffe383c03
Kirill Yakimovich <kirill.yakimovich@gmail.com>
2017-03-15 16:56:09 +0300

    [UPD] used fonts

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Fonts                                                                       #
+for font in ${PWD}/fonts/*; do
+    if [[ ! -e "/Library/Fonts/$(basename "$font")" ]]; then
+        printf "Copying %s to your Fonts folder\n" "$(basename "$font")"
+        sudo cp -a "$font"  /Library/Fonts
+    fi
+done
+
+# Homebrew
 if ! which brew &> /dev/null; then
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"	
 else
