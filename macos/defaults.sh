cb32e1187968e397bedf733fb4a12dca3264af37
Kirill Yakimovich <kirill.yakimovich@gmail.com>
2025-08-05 11:56:29 +0200

    Update keyboard repeat rate

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -19,6 +19,13 @@ sudo -v
 # Keep-alive: update existing 'sudo' time stamp until '.osx' has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Set a pretty fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+# Don't display the annoying prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
 # Set computer name (as done via System Preferences → Sharing)
 name=$1
 sudo scutil --set ComputerName "$name"
