0fdd96d0b6db6881f3ac6b4804c7d7fa6508dae9
Kirill Yakimovich <kirill.yakimovich@gmail.com>
2025-08-05 12:55:51 +0200

    Update brew management

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -1,24 +1,16 @@
-#!/bin/bash
-
 # Fonts                                                                       
 # FiraCode 2.6: https://github.com/tonsky/FiraCode/releases/tag/6.2
 for font in ${PWD}/fonts/*; do
     if [[ ! -e "/Library/Fonts/$(basename "$font")" ]]; then
         printf "Copying %s to your Fonts folder\n" "$(basename "$font")"
-        sudo cp -a "$font"  /Library/Fonts
+        cp -a "$font"  /Library/Fonts
     fi
 done
 
 # Homebrew
-if ! which brew &> /dev/null; then
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"	
-else
-	brew update --verbose
+if ! command -v brew &> /dev/null; then
+  echo "error: you need to install homebrew" >&2
+  exit 1
 fi
 
-#open "$DOTFILES/macos/defaults.sh"
-
-brew tap Homebrew/bundle
-brew bundle --file="$DOTFILES/macos/Brewfile"
-brew bundle --file="$DOTFILES/macos/Brewfile.cask"
-
+brew bundle --file="./macos/Brewfile"
